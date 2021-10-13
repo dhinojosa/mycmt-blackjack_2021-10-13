42de6c0bdf9ebe0729c9c877931cbe37bcee2b49
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-10-13 02:44:14 -0600

    Commit Lab #2

---
+++ src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
@@ -0,0 +1,25 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HandDisplayTest {
+    @Test
+    public void displayFirstCard() {
+        Hand hand = new Hand(List.of(new Card(Suit.HEARTS, Rank.ACE)));
+
+        assertThat(ConsoleHand.displayFirstCard(hand))
+                .isEqualTo("[31m┌─────────┐[1B[11D│A        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        A│[1B[11D└─────────┘");
+    }
+
+
+    @Test
+    public void displayHand() {
+        Hand hand = new Hand(List.of(new Card(Suit.HEARTS, Rank.ACE), new Card(Suit.CLUBS, Rank.TWO)));
+        assertThat(ConsoleHand.cardsAsString(hand))
+                .isEqualTo("[31m┌─────────┐[1B[11D│A        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        A│[1B[11D└─────────┘[6A[1C[30m┌─────────┐[1B[11D│2        │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│        2│[1B[11D└─────────┘");
+    }
+}
